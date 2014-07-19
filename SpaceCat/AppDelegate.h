ca3f54fa4ed6bfe092d2f4c2ee4f517315584e70
jianyaoang <jianyaoang1@gmail.com>
2014-07-19 13:04:07 -0500

    Adding files

---
+++ SpaceCat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpaceCat
+//
+//  Created by Jian Yao Ang on 7/19/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
