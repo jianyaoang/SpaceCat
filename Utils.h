c0df83370980ee8ac6a150411841a0bcd0a5a990
jianyaoang <jianyaoang1@gmail.com>
2014-07-20 17:54:03 -0500

    Created Utils class to hold all constant value

---
+++ Utils.h
@@ -0,0 +1,15 @@
+//
+//  Utils.h
+//  SpaceCat
+//
+//  Created by Jian Yao Ang on 7/20/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static const int ProjectileSpeed = 400;
+
+@interface Utils : NSObject
+
+@end
---
+++ Utils.m
@@ -0,0 +1,13 @@
+//
+//  Utils.m
+//  SpaceCat
+//
+//  Created by Jian Yao Ang on 7/20/14.
+//  Copyright (c) 2014 Jian Yao Ang. All rights reserved.
+//
+
+#import "Utils.h"
+
+@implementation Utils
+
+@end
